244b62812fe23ed97beb83e9f2a535073ea81c1c
mingyu1285 <mingyu1285@naver.com>
2024-05-16 21:55:53 +0900

    5/16 Test

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int a[][] = new int[2][3];
+
+        System.out.println("a.length : "+ a.length);
+        System.out.println("a[0].length : "+ a[0].length);
+    }
+}
