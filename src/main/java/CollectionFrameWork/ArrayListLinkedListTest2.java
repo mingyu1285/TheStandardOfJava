1d77f38957fe0de5a15c700f7b946b431b7bb597
mingyu1285 <mingyu1285@naver.com>
2024-07-14 22:08:09 +0900

    1/16 LinkedList에 대해 알아봤다.

--- src/main/java/CollectionFrameWork/ArrayListLinkedListTest2.java
+++ src/main/java/CollectionFrameWork/ArrayListLinkedListTest2.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * p.602
  * 배열의 경우 만일 인덱스가 n인 요소의 값을 얻어 오고자 한다면 단순히 아래와 같은 수식을 계산함으로써 해결된다.
- * 인덱스가 n인 데이터의 주소 = 배열의 주소 + n * 데이터 타입의 크기
+ * 인덱스가 n인 데이터의 주소 = 배열의 주소 + n * 데이터 타입의 크기이다.
  */
 public class ArrayListLinkedListTest2 {
     public static void main(String[] args) {
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,18 @@
 public class Test {
     public static void main(String[] args) {
-        int a[][] = new int[2][3];
+        int a,b,c,d;
 
-        System.out.println("a.length : "+ a.length);
-        System.out.println("a[0].length : "+ a[0].length);
+        a = b = 5;
+
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+
+
+        c = --a % --a;
+
+        System.out.println("c = "+ c);
+        d = ++b;
+
+        System.out.println("d = "+ d);
     }
 }
